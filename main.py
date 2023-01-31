e15314a791fc81524880dcb747c4994d4c2fcd9e
fureszb <fureszb@gmail.com>
2023-01-31 10:35:35 +0100

    kezdes

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #megoldás
 def eredmeny():
+    gpont = 0
+    jpont = 0
+    if gpont > 21:
+        print("A gép vesztett!")
+    elif jpont > 21:
+        print("A jatekos vesztett!")
     #tesztesetek
